cdca6ae29bbcc705d1946a7a57939a355a723b93
axa-uk-test\Richard Lai <richard.lai@health-on-line.co.uk>
2017-08-10 16:18:02 +0100

    with provider

--- SBClient/SBClient/Program.cs
+++ SBClient/SBClient/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,14 +16,15 @@ namespace SBClient
             var _messagingFactory = MessagingFactory.Create();
             var _namespaceManager = NamespaceManager.Create();
 
+            _namespaceManager.Settings.TokenProvider = TokenProvider.CreateOAuthTokenProvider(new Uri[] { new Uri("https://richlaisbhost:9355/ServiceBusDefaultNamespace") }, new NetworkCredential("richard.lai", "I.am.a.pirate101"));
+            _messagingFactory.GetSettings().TokenProvider = TokenProvider.CreateOAuthTokenProvider(new Uri[] { new Uri("https://richlaisbhost:9355/ServiceBusDefaultNamespace") }, new NetworkCredential("richard.lai", "I.am.a.pirate101"));
 
-
-            //Console.WriteLine("Checking");
-            //if (!_namespaceManager.QueueExists("MyQueue"))
-            //{
-            //    Console.WriteLine("Creating");
-            //    _namespaceManager.CreateQueue("MyQueue");
-            //}
+            Console.WriteLine("Checking");
+            if (!_namespaceManager.QueueExists("MyQueue"))
+            {
+                Console.WriteLine("Creating");
+                _namespaceManager.CreateQueue("MyQueue");
+            }
 
             QueueClient client = _messagingFactory.CreateQueueClient("MyQueue");
             client.Send(new BrokeredMessage("aha"));
