dffd570fb8f8ca23fe30a29e2d2bb1bd377c85a6
axa-uk-test\Richard Lai <richard.lai@health-on-line.co.uk>
2017-08-10 16:07:44 +0100

    send message

--- SBClient/SBClient/Program.cs
+++ SBClient/SBClient/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.ServiceBus.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,32 +11,21 @@ namespace SBClient
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Connecting");
-            //var _messagingFactory = MessagingFactory.Create();
-            //var _namespaceManager = NamespaceManager.Create();
+        {Console.WriteLine("Connecting");
+            var _messagingFactory = MessagingFactory.Create();
+            var _namespaceManager = NamespaceManager.Create();
 
-            NetworkCredential credential = new NetworkCredential("richard.lai", "I.am.a.pirate101");
-            var sbUriList = new List<Uri>() { new UriBuilder { Scheme = "sb", Host = "richlaisbhost", Path = "ServiceBusDefaultNamespace" }.Uri };
 
-            var httpsUriList = new List<Uri>() { new UriBuilder { Scheme = "https", Host = "richlaisbhost", Path = "ServiceBusDefaultNamespace", Port = 9355 }.Uri };
-            TokenProvider tokenProvider = TokenProvider.CreateOAuthTokenProvider(httpsUriList, credential);
 
-            MessagingFactory messagingFactory = MessagingFactory.Create(sbUriList, tokenProvider);
+            //Console.WriteLine("Checking");
+            //if (!_namespaceManager.QueueExists("MyQueue"))
+            //{
+            //    Console.WriteLine("Creating");
+            //    _namespaceManager.CreateQueue("MyQueue");
+            //}
 
-            ServiceBusConnectionStringBuilder connBuilder = new ServiceBusConnectionStringBuilder { ManagementPort = 9355, RuntimePort = 9354 };
-            connBuilder.Endpoints.Add(sbUriList[0]);
-            connBuilder.StsEndpoints.Add(httpsUriList[0]);
-
-            NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(connBuilder.ToString());
-            namespaceManager.Settings.TokenProvider = tokenProvider;
-
-            Console.WriteLine("Checking");
-            if (!namespaceManager.QueueExists("MyQueue"))
-            {
-                Console.WriteLine("Creating");
-                namespaceManager.CreateQueue("MyQueue");
-            }
+            QueueClient client = _messagingFactory.CreateQueueClient("MyQueue");
+            client.Send(new BrokeredMessage("aha"));
 
             Console.WriteLine("Finished");
         }
